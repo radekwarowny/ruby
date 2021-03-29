3563f376bd7515c640dcfa0a5134f98eccd3cf03
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2021-03-29 10:30:05 +0100

    Add files via upload

--- tutorialspoint/classes_objects.rb
+++ tutorialspoint/classes_objects.rb
@@ -67,3 +67,38 @@ cust1.total_no_of_customers()
 
 cust2.display_details()
 cust2.total_no_of_customers()
+
+# Ruby Global Variables
+
+$global_variable = 10
+
+class Class1
+	def print_global
+		puts "Global variable in Class1 is #$global_variable"
+	end
+end
+
+class Class2
+	def print_global
+		puts "Global variable in Class2 is #$global_variable"
+	end
+end
+
+class1obj = Class1.new
+class1obj.print_global
+class2obj = Class2.new
+class2obj.print_global
+
+# Ruby Constants
+
+class Example
+	VAR1 = 100
+	VAR2 = 200
+	def show
+		puts "Value of first Constant is #{VAR1}"
+		puts "Value of second Constant is #{VAR2}"
+	end
+end
+
+object = Example.new()
+object.show
