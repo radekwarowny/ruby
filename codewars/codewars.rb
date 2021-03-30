c846fc929917ab140260eae04571937539dda505
Radek Warowny <65827302+radekwarowny@users.noreply.github.com>
2021-03-30 10:04:39 +0100

    Add files via upload

--- codewars/codewars.rb
+++ codewars/codewars.rb
@@ -110,11 +110,74 @@ def digitize(n)
 			break
 		end
 	end
-	print arr
 	result = arr.reverse()
-	print(result)
+	result = result.map(&:to_i)
+	print result
+	return result
+
 end
 
 digitize(348597)
+
+
+# Exercise 5
+
+=begin
+I have a cat and a dog.
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+    humanYears >= 1
+    humanYears are whole numbers only
+
+Cat Years
+
+    15 cat years for first year
+    +9 cat years for second year
+    +4 cat years for each year after that
+
+Dog Years
+
+    15 dog years for first year
+    +9 dog years for second year
+    +5 dog years for each year after that
+
+	
+=end
+
+print "\n\n"
+print "Cat years, Dog years"
+print "\n"
+
+def human_years_cat_years_dog_years(human_years)
+
+	cat_years = 0
+	dog_years = 0
+
+	i = 1
+
+	while i < (human_years + 1) do
+		if i == 1
+			cat_years += 15
+			dog_years += 15
+		elsif i == 2 
+			cat_years += 9
+			dog_years += 9
+		elsif i > 2
+			cat_years += 4
+			dog_years += 5
+		else 
+			puts "Input Error"
+		end
+
+		i += 1
+	end
+	puts human_years, cat_years, dog_years
+	return [human_years, cat_years, dog_years]
+end
+
+human_years_cat_years_dog_years(10)
 
 
